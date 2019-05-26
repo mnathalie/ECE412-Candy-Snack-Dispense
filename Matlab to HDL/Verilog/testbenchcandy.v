8c7a64f39129ed2ec5b14646a5b25cf135900099
Jenn5 <jennlara@pdx.edu>
2019-05-26 10:29:11 -0700

    Add files via upload

--- Matlab to HDL/Verilog/testbenchcandy.v
+++ Matlab to HDL/Verilog/testbenchcandy.v
@@ -19,10 +19,10 @@ module testbench;
 		wire 		handshake;		//output
 		wire 		clock_DC;
 		wire 		clock_DCSLOW;
-		wire 		clock_test_step; 
+		wire		[7:0] countermax_test; 
 project_module DUT( .rstn(rstn),.IO_B4(teststate0),.IO_B5(teststate1),.IO_B6(teststate2),
 					.IO_A3(stateamount0),.IO_A4(stateamount1),.IO_A5(candyflag),.IO_D9(stepperstep),.IO_A10(stepperdir),
-					.IO_B9(dcmotor0),.IO_F7(dcmotor1), .IO_C4(dcmotor2), .IO_D6(handshake) ,.clock_test_DC(clock_DC),.clock_test_DCSLOW(clock_DCSLOW), .clock_test_step(clock_test_step)
+					.IO_B9(dcmotor0),.IO_F7(dcmotor1), .IO_C4(dcmotor2), .IO_D6(handshake) ,.clockhandshake(clockhandshake),.clock_test_DCSLOW(clock_DCSLOW), .countermax_test(countermax_test)
 				   );
 /*	//assign TESTSTATE = {teststate0,teststate1,teststate2} ;
 		#5 rstn = 1'b0;	
@@ -90,29 +90,39 @@ project_module DUT( .rstn(rstn),.IO_B4(teststate0),.IO_B5(teststate1),.IO_B6(tes
 			
 		#5 rstn = 1'b0;	
 		#2 rstn = 1'b1;
-		#10 // .1s
+		#5 // .1s  
+		candyflag = 1'b0;
+		stateamount0 = 1'b1;
+		stateamount1 = 1'b1;
+		#5
 		stateamount0 = 1'b0;
 		stateamount1 = 1'b0;
 		candyflag = 1'b1;
-		#60
-		//if(handshake == 1)
-		//	begin
-		//		#10
-		candyflag = 1'b0; 
-		//	end	
-		#10
+		#25
+		if(handshake == 1)
+			candyflag = 1'b0; 
+		#5
 		stateamount0 = 1'b1;
 		stateamount1 = 1'b0;
 		candyflag = 1'b1;
-		#60
-		candyflag = 1'b0;
-		#8
+		#25
+		if(handshake == 1) 
+			candyflag = 1'b0; 
+		#5
+
 		stateamount0 = 1'b0;
 		stateamount1 = 1'b1;
 		candyflag = 1'b1;
-		#60		
-		candyflag = 1'b0;
-		#8
+		#25		
+		if(handshake == 1)	   
+				candyflag = 1'b0; 
+		#5
+		stateamount0 = 1'b0;
+		stateamount1 = 1'b0;
+		candyflag = 1'b1;
+		#25		
+		if(handshake == 1)	   
+				candyflag = 1'b0; 
 		//#90000 // 9s
 		$finish; 
 	end
